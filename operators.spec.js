0bc42b26f5542e0864eae78f47ddad04f43354e2
maruthu1998 <maruthupandi201998@gamil.com>
2025-05-06 09:45:03 -0700

    Added one more file for Operators in JavaScript

---
+++ operators.spec.js
@@ -0,0 +1,11 @@
+var a=22;
+let c=39;
+const k=89;
+let m=11;
+//m=99;
+if(m<c){  
+ a=c; // if m is less than c, assign c to a
+}else{
+  a=m; // if m is greater than c, asssign m to a
+}
+console.log('the value of a is :', a);
