89fc1f21dcff09465848422eae79d9f810b699e6
maruthu1998 <maruthupandi201998@gamil.com>
2025-05-08 04:33:41 -0700

    Added array in this program

--- ifCondition.spec.js
+++ ifCondition.spec.js
@@ -7,6 +7,9 @@ for(a=yearOfBirth;a<=currentYear;a++){
  let year=a%4==0 || (a%100==0 && a%400==0);
  if(year){
     console.log(a);
+    let arr=new Array(8);
+    for(i=0;i<=7;i++){
+      arr[i]=[a];
+}
  }
-
-}       
+ }       
